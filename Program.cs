efc843d1fa10e7a07131ea7086588d9fb74071b2
ErikTanis <eriktanis21@gmail.com>
2024-11-27 13:23:34 +0100

    Fetch data from API

--- Program.cs
+++ Program.cs
@@ -13,8 +13,6 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
